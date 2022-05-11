89a6372eb5ca2a5754c2c15ba5bb6181dfa78c8f
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2022-05-11 12:23:56 +0530

    recommender-engine-service initial structure

---
+++ recommender-engine-service/src/main/java/com/aclusive/recommenderengineservice/RecommenderEngineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aclusive.recommenderengineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommenderEngineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommenderEngineServiceApplication.class, args);
+	}
+
+}
---
+++ recommender-engine-service/src/test/java/com/aclusive/recommenderengineservice/RecommenderEngineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aclusive.recommenderengineservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommenderEngineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
