7ea820fef47f709b4c62307fa94ac7fba148ea3e
Sahan Kariyawasam <68751902+sahan-98@users.noreply.github.com>
2022-05-14 01:30:26 +0530

    Basic Spring boot setting

---
+++ product_catlog_service_engine/src/test/java/ctse/product_catlog_service_engine/ProductCatlogServiceEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package ctse.product_catlog_service_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatlogServiceEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
