96ed0db173d8457ed98e448e17565a8803e9db76
MSI PC <navodinith@gmail.com>
2022-05-16 10:40:34 +0530

    repo created

---
+++ user-service/src/main/java/com/aclusive/userservice/repository/UserDetailsRepository.java
@@ -0,0 +1,10 @@
+package com.aclusive.userservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.aclusive.userservice.entity.UserDetails;
+
+@Repository
+public interface UserDetailsRepository extends JpaRepository<UserDetails, Long> {
+}
